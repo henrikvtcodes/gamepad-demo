aacac0c4bd1ac598c85929965527a684b696e3c7
henrikvtcodes <commits@henrikvt.com>
2024-01-22 19:38:56 -0500

    prelim gamepad state in zustand

--- src/utils/gamepadState.ts
+++ src/utils/gamepadState.ts
@@ -1,3 +1,27 @@
 import { atom } from "jotai";
+import { create } from "zustand";
 
 export const selectedGamepad = atom<Gamepad | null>(null);
+
+interface GamepadState {
+  // State
+  selectedGamepad: Gamepad | null;
+  gamepadState: {
+    buttons: GamepadButton[];
+    axes: number[];
+  } | null;
+
+  // Actions
+  setGamepad: (gamepad: Gamepad) => void;
+  clearGamepad: () => void;
+}
+
+export const useGamepadState = create<GamepadState>()((set) => ({
+  // State
+  selectedGamepad: null,
+  gamepadState: null,
+
+  // Actions
+  setGamepad: (gamepad) => set({ selectedGamepad: gamepad }),
+  clearGamepad: () => set({ selectedGamepad: null }),
+}));
